9656393f85c4909f3bf9edad2016048a3bc1737a
Joel Jacob <joel.panvel@gmail.com>
2025-09-22 23:49:53 +0530

    Learning about attribute selector in angular

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
 import {Component} from "@angular/core";
+import {ButtonComponent} from "../shared/button/button.component";
 
 
 @Component({
-    selector : 'app-header',
-    standalone : true,
-    templateUrl : './header.component.html',
-    styleUrls : ['./header.component.css'],
-    imports : []
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
+    imports: [ButtonComponent]
 })
 
-export class HeaderComponent {}
+export class HeaderComponent {
+}
---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector : 'button[appButton]',
+    templateUrl: './button.component.html',
+    styleUrls : ['./button.component.css'],
+    standalone : true,
+    imports : []
+})
+
+export class ButtonComponent {}
