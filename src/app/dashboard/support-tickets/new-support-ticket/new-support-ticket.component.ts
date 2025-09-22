2f6067f3caaa325aead7946a208e623894694267
Joel Jacob <joel.panvel@gmail.com>
2025-09-22 23:54:16 +0530

    Using ng-content projection in multiple places

--- src/app/dashboard/support-tickets/new-support-ticket/new-support-ticket.component.ts
+++ src/app/dashboard/support-tickets/new-support-ticket/new-support-ticket.component.ts
@@ -1,9 +1,12 @@
 import {Component} from "@angular/core";
+import {ButtonComponent} from "../../../shared/button/button.component";
 
 @Component({
     selector : 'app-new-support-ticket',
     standalone : true,
-    imports : [],
+    imports: [
+        ButtonComponent
+    ],
     templateUrl : './new-support-ticket.component.html',
     styleUrls : ['./new-support-ticket.component.css']
 })
