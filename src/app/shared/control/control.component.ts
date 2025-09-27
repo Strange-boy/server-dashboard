84ec5303db400fa6ad9d6ded836544ac7a0ab02b
Joel Jacob <joel.panvel@gmail.com>
2025-09-27 20:22:46 +0530

    Experimenting view encapsulation in angular

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, ViewEncapsulation} from "@angular/core";
 
 @Component({
     selector : 'app-control',
     templateUrl : './control.component.html',
     styleUrls : ['./control.component.css'],
     standalone : true,
-    imports : []
+    imports : [],
+    encapsulation: ViewEncapsulation.None
 })
 
 export class ControlComponent {
